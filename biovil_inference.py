4a541643a1297f47ad56c52a48332551dcdbfe28
Rajat Mittal <rajatmittal@college.harvard.edu>
2022-12-02 17:08:37 +0000

    Integrate BioViL Vision Stack into CheXzero Code
    Added a flag called use_biovision and added in logic to load the BioViL
    ResNet model which we are calling BioVision.

--- biovil_inference.py
+++ biovil_inference.py
@@ -1,6 +1,1 @@
-# %%
-from health_multimodal.image import get_biovil_resnet_inference
-
-# %% 
-inference_engine = get_biovil_resnet_inference()
-# %%
+from health_multimodal.image import get_biovil_resnet
