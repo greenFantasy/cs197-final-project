7e05a5b62f37a013552e705d38bc5525610c0e5f
Kostas Tingos <kostastingos@college.harvard.edu>
2022-12-05 02:51:06 +0000

    Add functionality to process the vindr dataset
    Before we had no way to process the vindr-cxr dataset since the image
    files were all dicom. This change adds in this functionality so the
    images can be processed for evaluation.

--- run_preprocess.py
+++ run_preprocess.py
@@ -1,6 +1,6 @@
 import argparse
 from pathlib import Path
-from data_process import get_cxr_paths_list, img_to_hdf5, get_cxr_path_csv, write_report_csv, biovil_img_to_hdf5
+from data_process import get_cxr_paths_list, img_to_hdf5, get_cxr_path_csv, write_report_csv, biovil_img_to_hdf5, dicom_img_to_hdf5
 from preprocess_padchest import img_to_h5
 
 
@@ -8,7 +8,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--csv_out_path', type=str, default='data/cxr_paths.csv', help="Directory to save paths to all chest x-ray images in dataset.")
     parser.add_argument('--cxr_out_path', type=str, default='data/cxr.h5', help="Directory to save processed chest x-ray image data.")
-    parser.add_argument('--dataset_type', type=str, default='mimic', choices=['mimic', 'chexpert-test', 'padchest-sample'], help="Type of dataset to pre-process")
+    parser.add_argument('--dataset_type', type=str, default='mimic', choices=['mimic', 'chexpert-test', 'padchest-sample', 'vindr'], help="Type of dataset to pre-process")
     parser.add_argument('--mimic_impressions_path', default='data/mimic_impressions.csv', help="Directory to save extracted impressions from radiology reports.")
     parser.add_argument('--chest_x_ray_path', default='/deep/group/data/mimic-cxr/mimic-cxr-jpg/2.0.0/files', help="Directory where chest x-ray image data is stored. This should point to the files folder from the MIMIC chest x-ray dataset.")
     parser.add_argument('--radiology_reports_path', default='/deep/group/data/med-data/files/', help="Directory radiology reports are stored. This should point to the files folder from the MIMIC radiology reports dataset.")
@@ -51,9 +51,7 @@ if __name__ == "__main__":
         saved_paths = img_to_h5(cxr_paths, args.cxr_out_path)
         print(f"{len(saved_paths)}/{len(cxr_paths)} imgs converted into h5")
 
-        
-        
-        
-    
-
-
+    elif args.dataset_type == 'vindr':    
+        get_cxr_path_csv(args.csv_out_path, args.chest_x_ray_path, file_extension='.dicom')
+        cxr_paths = get_cxr_paths_list(args.csv_out_path)
+        dicom_img_to_hdf5(cxr_paths, args.cxr_out_path, resolution=224)
